eb5ec334782bf3f3245665b3f9966984d989c436
Enzo Pain <nozbead@outlook.com>
2021-10-12 15:24:05 +0200

    maven init

---
+++ src/main/java/fr/funetdelire/App.java
@@ -0,0 +1,13 @@
+package fr.funetdelire;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
